7b17a3c65fab235e85490ae1449f8e03d9be5fad
Josh Tari <josh.tari@dogmacompany.com>
2018-09-21 23:32:47 +0200

    [Bugfix] only copy newer files under #Windows

--- windows/post_build.bat
+++ windows/post_build.bat
@@ -1,5 +1,5 @@
-xcopy /E /Y /I ..\..\addons\libtari\assets\effects %1\effects
-xcopy /E /Y /I ..\..\addons\libtari\assets\logo %1\logo
-xcopy /E /Y /I ..\..\addons\libtari\assets\debug %1\debug
-xcopy /E /Y /I ..\..\addons\libtari\assets\fonts %1\fonts
-xcopy /E /I /Y ..\assets %1\assets
+xcopy /E /Y /I /D ..\..\addons\libtari\assets\effects %1\effects
+xcopy /E /Y /I /D ..\..\addons\libtari\assets\logo %1\logo
+xcopy /E /Y /I /D ..\..\addons\libtari\assets\debug %1\debug
+xcopy /E /Y /I /D ..\..\addons\libtari\assets\fonts %1\fonts
+xcopy /E /I /Y /D ..\assets %1\assets
