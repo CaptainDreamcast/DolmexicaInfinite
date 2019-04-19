b5aa6e8ce0cc31c99ff267f3e240c5e923dbe2c4
Josh Tari <josh.tari@dogmacompany.com>
2019-04-20 01:50:22 +0200

    [Refactoring] move output directory to root

--- windows/post_build.bat
+++
@@ -1,5 +0,0 @@
-xcopy /E /Y /I /D ..\..\addons\libtari\assets\effects %1\effects
-xcopy /E /Y /I /D ..\..\addons\libtari\assets\logo %1\logo
-xcopy /E /Y /I /D ..\..\addons\libtari\assets\debug %1\debug
-xcopy /E /Y /I /D ..\..\addons\libtari\assets\fonts %1\fonts
-xcopy /E /I /Y /D ..\assets %1\assets
